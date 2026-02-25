af594c20b28a20fcb35eaf5ee5f46d88113dfa9c
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2026-02-25 20:55:49 +0100

    Add CSS styles for body and active navigation links

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
-import "./styles.css";
+import "./assets/css/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
